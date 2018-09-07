f86642daddf964e3f78a5f3fd4282d6394eebd22
yoogo <yoogoc@163.com>
2018-09-07 11:22:04 +0800

    remove gem

--- template.rb
+++ template.rb
@@ -1,6 +1,6 @@
 # ----- Add Gem ---------------------------------------------
-gem 'redis'
-gem 'bcrypt'
+# gem 'redis'
+# gem 'bcrypt'
 gem 'mini_magick'
 # gem 'bootstrap-sass'
 # gem 'font-awesome-sass'
@@ -11,40 +11,40 @@ gem 'mini_magick'
 # gem 'jquery-cookie-rails'
 gem 'rest-client'
 gem 'whenever'
-gem 'will_paginate'
-gem 'will_paginate-bootstrap'
+# gem 'will_paginate'
+# gem 'will_paginate-bootstrap'
 gem 'kaminari'
-gem "ransack"
-gem 'carrierwave'
-gem "paperclip"
+# gem "ransack"
+# gem 'carrierwave'
+# gem "paperclip"
 # gem 'momentjs-rails'
 # gem 'bootstrap3-datetimepicker-rails'
-gem "simple_form"
+# gem "simple_form"
 # gem 'jquery-fileupload-rails'
-gem 'redis-rails'
-gem 'redis-namespace'
+# gem 'redis-rails'
+# gem 'redis-namespace'
 gem 'paper_trail'
-gem "apipie-rails"
-gem 'ckeditor'
+# gem "apipie-rails"
+# gem 'ckeditor'
 gem_group :development, :test do
-  gem "rspec-rails"
+  # gem "rspec-rails"
   gem "pry-rails"
   gem 'pry-doc'
   gem 'pry-byebug'
-  gem "quiet_assets"
+  # gem "quiet_assets"
   gem "better_errors"
-  gem "binding_of_caller"
-  gem 'guard-rspec'
-  gem 'spring-commands-rspec'
+  # gem "binding_of_caller"
+  # gem 'guard-rspec'
+  # gem 'spring-commands-rspec'
   gem 'did_you_mean'
   gem 'faker'
 end
 # ----- Remove CoffeeScript, Sass and "all that jazz" ---------------------------------------------
 
-comment_lines 'Gemfile', /gem 'coffee/
-comment_lines 'Gemfile', /gem 'sass/
-comment_lines 'Gemfile', /gem 'uglifier/
-uncomment_lines 'Gemfile', /gem 'therubyracer/
+# comment_lines 'Gemfile', /gem 'coffee/
+# comment_lines 'Gemfile', /gem 'sass/
+# comment_lines 'Gemfile', /gem 'uglifier/
+# uncomment_lines 'Gemfile', /gem 'therubyracer/
 
 application "config.generators do |g|
       g.test_framework  false
